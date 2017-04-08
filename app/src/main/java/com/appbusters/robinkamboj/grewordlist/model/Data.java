8c11b10318e05c94af1a414588226992c35db573
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-08 15:48:02 +0530

    row layout complete.

---
+++ app/src/main/java/com/appbusters/robinkamboj/grewordlist/model/Data.java
@@ -0,0 +1,15 @@
+package com.appbusters.robinkamboj.grewordlist.model;
+
+public class Data {
+
+    private String word;
+    private String meaning;
+    private String example;
+
+    public Data(String word, String meaning, String example) {
+        this.word = word;
+        this.meaning = meaning;
+        this.example = example;
+    }
+    
+}
