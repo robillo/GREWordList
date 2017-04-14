61bde64364d18a1b790e4b2d0686240c1a978acd
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-14 18:41:03 +0530

    FlashCardsFragment.

--- app/src/main/java/com/appbusters/robinkamboj/grewordlist/view/fragments/FlashCardsFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/grewordlist/view/fragments/FlashCardsFragment.java
@@ -1,13 +1,19 @@
 package com.appbusters.robinkamboj.grewordlist.view.fragments;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.appbusters.robinkamboj.grewordlist.R;
 import com.appbusters.robinkamboj.grewordlist.controller.FlashCardsAdapter;
@@ -24,6 +30,9 @@ public class FlashCardsFragment extends Fragment {
     private RecyclerView recyclerView;
     private FlashCardsAdapter adapter;
     private List<String> data;
+    private CharSequence[] items = {"Level 1 (4 hints)", "Level 2 (8 hints)", "Level 3 (12 hints)", "Level 4 (16 hints)", "Level 5 (20 hints)"};
+    private String[] meaningsList;
+    private String word;
 
     public FlashCardsFragment() {
         // Required empty public constructor
@@ -35,6 +44,7 @@ public class FlashCardsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_flash_cards, container, false);
+        setHasOptionsMenu(true);
 
         data = fillWithData();
 
@@ -44,6 +54,55 @@ public class FlashCardsFragment extends Fragment {
         recyclerView.setAdapter(adapter);
 
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_flash, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.flash_filter:{
+                Toast.makeText(getActivity(), "Flash Menu Filter", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Choose Level:")
+                        .setItems(items, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                switch (which){
+                                    case 0:{
+
+                                        break;
+                                    }
+                                    case 1:{
+
+                                        break;
+                                    }
+                                    case 2:{
+
+                                        break;
+                                    }
+                                    case 3:{
+
+                                        break;
+                                    }
+                                    case 4:{
+
+                                        break;
+                                    }
+                                }
+                            }
+                        });
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private List<String> fillWithData(){
